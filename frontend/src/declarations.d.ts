424fafe36408032da9a0c0550f884197b106c93d
Brandon Tan <brandon363058@gmail.com>
2024-08-22 12:01:55 -0500

    more declarations

--- frontend/src/declarations.d.ts
+++ frontend/src/declarations.d.ts
@@ -1,2 +1,5 @@
+declare module 'react-dom/client';
+declare module 'vite';
+declare module '@vitejs/plugin-react-swc';
 declare module 'react-syntax-highlighter';
 declare module 'react-syntax-highlighter/dist/esm/styles/prism';
