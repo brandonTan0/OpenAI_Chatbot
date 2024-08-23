b4a9699d87be7d818207b21c8a7dc35b230cc227
Brandon Tan <brandon363058@gmail.com>
2024-08-23 10:12:41 -0500

    Minor changes to build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { createTheme, ThemeProvider } from '@mui/material';
+//import { createTheme, ThemeProvider } from '@mui/material';
 //import { BrowserRouter } from 'react-router-dom';
 import { HashRouter as Router } from 'react-router-dom';
 
 import { AuthProvider } from './context/AuthContext';
 import axios from 'axios';
-import { Toaster } from 'react-hot-toast';
+//import { Toaster } from 'react-hot-toast';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 if (process.env.NODE_ENV === 'production') disableReactDevTools();
@@ -16,19 +16,19 @@ if (process.env.NODE_ENV === 'production') disableReactDevTools();
 axios.defaults.baseURL = 'https://openai-chatbot-api.onrender.com/api/v1';
 axios.defaults.withCredentials = true;
 
-const theme = createTheme({
+/*const theme = createTheme({
   typography: {
     fontFamily: "Roboto Slab, serif",
     allVariants: { color: 'white' },
   }
-});
+});*/
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <Router>
-        
-          
+        {/*<ThemeProvider theme = { theme }>
+          <Toaster position='top-center' />*/}
           <App />
         
       </Router>
