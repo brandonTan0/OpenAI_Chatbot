0ddf9086f468cccf003ea8d42c713374b3533491
Brandon Tan <brandon363058@gmail.com>
2024-08-23 09:59:02 -0500

    Confirming client-side works

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { createTheme, ThemeProvider } from '@mui/material';
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
+
 import { AuthProvider } from './context/AuthContext';
 import axios from 'axios';
 import { Toaster } from 'react-hot-toast';
@@ -24,12 +26,12 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <Router>
         <ThemeProvider theme = { theme }>
           <Toaster position='top-center' />
           <App />
         </ThemeProvider>
-      </BrowserRouter>
+      </Router>
     </AuthProvider>
   </React.StrictMode>
 );
