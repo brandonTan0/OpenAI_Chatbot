69ee9a299b88e724c5ca4a559bb204c1858c6c15
Brandon Tan <brandon363058@gmail.com>
2024-08-23 07:44:03 -0500

    Updated baseURL

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,7 +11,7 @@ import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
-axios.defaults.baseURL = 'http://localhost:3000/api/v1';
+axios.defaults.baseURL = 'https://openai-chatbot-j6t5.onrender.com/api/v1';
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
