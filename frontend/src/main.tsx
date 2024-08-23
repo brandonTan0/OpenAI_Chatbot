ed72ccb731d009d7363812f56de8c612cac0c2f4
Brandon Tan <brandon363058@gmail.com>
2024-08-23 10:28:46 -0500

    Testing deploy

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+//import App from './App';
 import './index.css';
 //import { createTheme, ThemeProvider } from '@mui/material';
 //import { BrowserRouter } from 'react-router-dom';
@@ -10,6 +10,7 @@ import { AuthProvider } from './context/AuthContext';
 import axios from 'axios';
 //import { Toaster } from 'react-hot-toast';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+import Home from './pages/Home';
 
 if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
@@ -28,10 +29,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <AuthProvider>
       <Router>
         {/*<ThemeProvider theme = { theme }>
-          <Toaster position='top-center' />*/}
-          <App />
+          <Toaster position='top-center' />
+          <App />*/}
+          <Home />
         
       </Router>
     </AuthProvider>
   </React.StrictMode>
 );
+
