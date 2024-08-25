110d66defe8d1e201c6d945ea63e37c354f9e314
Brandon Tan <brandon363058@gmail.com>
2024-08-25 08:14:52 -0500

    Finalizing backend path for deployment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ import { Toaster } from 'react-hot-toast';
 
 
 
-axios.defaults.baseURL = 'http://localhost:3000/api/v1';
+axios.defaults.baseURL = 'https://openai-chatbot-api.onrender.com/api/v1';
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
